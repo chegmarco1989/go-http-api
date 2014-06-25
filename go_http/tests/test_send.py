8696c13e01f269c60d654fefa5730ce02df24b08
Simon Cross <hodgestar@gmail.com>
2014-06-25 17:44:50 +0200

    Tests for HttpApiSender.

---
+++ go_http/tests/test_send.py
@@ -0,0 +1,87 @@
+""" Tests for go_http.send. """
+
+import json
+from unittest import TestCase
+
+from requests_testadapter import TestAdapter, TestSession
+
+from go_http.send import HttpApiSender
+
+
+class RecordingAdapter(TestAdapter):
+    """ Record the request that was handled by the adapter.
+    """
+    request = None
+
+    def send(self, request, *args, **kw):
+        self.request = request
+        return super(RecordingAdapter, self).send(request, *args, **kw)
+
+
+class TestHttpApiSender(TestCase):
+    def setUp(self):
+        self.session = TestSession()
+        self.sender = HttpApiSender(
+            account_key="acc-key", conversation_key="conv-key",
+            conversation_token="conv-token", session=self.session)
+
+    def check_request(self, request, method, data=None, headers=None):
+        self.assertEqual(request.method, method)
+        if data is not None:
+            self.assertEqual(json.loads(request.body), data)
+        if headers is not None:
+            for key, value in headers.items():
+                self.assertEqual(request.headers[key], value)
+
+    def test_send_text(self):
+        adapter = RecordingAdapter(json.dumps({"message_id": "id-1"}))
+        self.session.mount(
+            "http://go.vumi.org/api/v1/go/http_api_nostream/conv-key/"
+            "messages.json", adapter)
+
+        result = self.sender.send_text("to-addr-1", "Hello!")
+        self.assertEqual(result, {
+            "message_id": "id-1",
+        })
+        self.check_request(
+            adapter.request, 'PUT',
+            data={"content": "Hello!", "to_addr": "to-addr-1"},
+            headers={"Authorization": u'Basic YWNjLWtleTpjb252LXRva2Vu'})
+
+    def test_fire_metric(self):
+        adapter = RecordingAdapter(
+            json.dumps({"success": True, "reason": "Yay"}))
+        self.session.mount(
+            "http://go.vumi.org/api/v1/go/http_api_nostream/conv-key/"
+            "metrics.json", adapter)
+
+        result = self.sender.fire_metric("metric-1", 5.1, agg="max")
+        self.assertEqual(result, {
+            "success": True,
+            "reason": "Yay",
+        })
+        self.check_request(
+            adapter.request, 'PUT',
+            data=[["metric-1", 5.1, "max"]],
+            headers={"Authorization": u'Basic YWNjLWtleTpjb252LXRva2Vu'})
+
+    def test_fire_metric_default_agg(self):
+        adapter = RecordingAdapter(
+            json.dumps({"success": True, "reason": "Yay"}))
+        self.session.mount(
+            "http://go.vumi.org/api/v1/go/http_api_nostream/conv-key/"
+            "metrics.json", adapter)
+
+        result = self.sender.fire_metric("metric-1", 5.2)
+        self.assertEqual(result, {
+            "success": True,
+            "reason": "Yay",
+        })
+        self.check_request(
+            adapter.request, 'PUT',
+            data=[["metric-1", 5.2, "last"]],
+            headers={"Authorization": u'Basic YWNjLWtleTpjb252LXRva2Vu'})
+
+
+class TestLoggingSender(TestCase):
+    pass
