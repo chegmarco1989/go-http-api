2b3a40a571be48e70e0036fdffec9c64f2b23857
Jeremy Thurgood <jeremy@praekelt.com>
2014-07-30 13:58:42 +0200

    Switch to fake-go-contacts for contacts API tests.

--- go_http/tests/test_contacts.py
+++ go_http/tests/test_contacts.py
@@ -2,130 +2,15 @@
 Tests for go_http.contacts.
 """
 
-import json
-from uuid import uuid4
 from unittest import TestCase
 
 from requests import HTTPError
 from requests.adapters import HTTPAdapter
 from requests_testadapter import TestSession, Resp, TestAdapter
 
+from fake_go_contacts import Request, FakeContactsApi
+
 from go_http.contacts import ContactsApiClient
-
-
-def make_contact_dict(fields):
-    contact = {
-        # Always generate a key. It can be overridden by `fields`.
-        u'key': uuid4().hex,
-
-        # Some constant-for-our-purposes fields.
-        u'$VERSION': 2,
-        u'user_account': u'owner-1',
-        u'created_at': u'2014-07-25 12:44:11.159151',
-
-        # Everything else.
-        u'name': None,
-        u'surname': None,
-        u'groups': [],
-        u'msisdn': None,
-        u'twitter_handle': None,
-        u'bbm_pin': None,
-        u'mxit_id': None,
-        u'dob': None,
-        u'facebook_id': None,
-        u'wechat_id': None,
-        u'email_address': None,
-        u'gtalk_id': None,
-    }
-    contact.update(fields)
-    return contact
-
-
-class FakeContactsError(Exception):
-    """
-    Error we can use to craft a different HTTP response.
-    """
-
-
-class FakeContactsApi(object):
-    """
-    Fake implementation of the Vumi Go contacts API.
-    """
-
-    def __init__(self, auth_token, contacts_data):
-        self.auth_token = auth_token
-        self.contacts_data = contacts_data
-
-    def handle_request(self, request):
-        if not self.check_auth(request):
-            return self.build_response("", 403)
-
-        # TODO: Improve this as our server implementation grows.
-
-        contact_key = request.path_url.replace("/go/contacts", "").lstrip("/")
-
-        try:
-            if not contact_key:
-                if request.method == "POST":
-                    return self.create_contact(request)
-                else:
-                    return self.build_response("", 405)
-
-            if request.method == "GET":
-                return self.get_contact(contact_key, request)
-            elif request.method == "PUT":
-                # NOTE: This is an incorrect use of the PUT method, but it's
-                # what we have for now.
-                return self.update_contact(contact_key, request)
-            elif request.method == "DELETE":
-                return self.delete_contact(contact_key, request)
-            else:
-                return self.build_response("", 405)
-
-        except FakeContactsError as err:
-            return self.build_response(*err.args)
-
-    def check_auth(self, request):
-        auth_header = request.headers.get("Authorization")
-        return auth_header == "Bearer %s" % (self.auth_token,)
-
-    def build_response(self, content, code=200, headers=None):
-        return Resp(content, code, headers)
-
-    def _get_contact(self, contact_key):
-        contact = self.contacts_data.get(contact_key)
-        if contact is None:
-            raise FakeContactsError("Contact not found.", 404)
-        return contact
-
-    def create_contact(self, request):
-        # TODO: Confirm this behaviour against the real API.
-        contact_data = json.loads(request.body)
-        if u"key" in contact_data:
-            raise FakeContactsError("", 400)
-        contact = make_contact_dict(contact_data)
-        self.contacts_data[contact[u"key"]] = contact
-        return self.build_response(json.dumps(contact))
-
-    def get_contact(self, contact_key, request):
-        # TODO: Confirm this behaviour against the real API.
-        contact = self._get_contact(contact_key)
-        return self.build_response(json.dumps(contact))
-
-    def update_contact(self, contact_key, request):
-        # TODO: Confirm this behaviour against the real API.
-        contact = self._get_contact(contact_key)
-        update_data = json.loads(request.body)
-        for k, v in update_data.iteritems():
-            # TODO: Reject changes to key?
-            contact[k] = v
-        return self.build_response(json.dumps(contact))
-
-    def delete_contact(self, contact_key, request):
-        # TODO: Confirm this behaviour against the real API.
-        contact = self._get_contact(contact_key)
-        self.contacts_data.pop(contact_key)
-        return self.build_response(json.dumps(contact))
 
 
 class FakeContactsApiAdapter(HTTPAdapter):
@@ -142,12 +27,18 @@ class FakeContactsApiAdapter(HTTPAdapter):
 
     def send(self, request, stream=False, timeout=None,
              verify=True, cert=None, proxies=None):
-        resp = self.contacts_api.handle_request(request)
-        r = self.build_response(request, resp)
+        req = Request(
+            request.method, request.path_url, request.body, request.headers)
+        resp = self.contacts_api.handle_request(req)
+        response = Resp(resp.body, resp.code, resp.headers)
+        r = self.build_response(request, response)
         if not stream:
             # force prefetching content unless streaming in use
             r.content
         return r
+
+
+make_contact_dict = FakeContactsApi.make_contact_dict
 
 
 class TestContactsApiClient(TestCase):
@@ -157,7 +48,7 @@ class TestContactsApiClient(TestCase):
     def setUp(self):
         self.contacts_data = {}
         self.contacts_backend = FakeContactsApi(
-            self.AUTH_TOKEN, self.contacts_data)
+            "go/", self.AUTH_TOKEN, self.contacts_data)
         self.session = TestSession()
         adapter = FakeContactsApiAdapter(self.contacts_backend)
         self.session.mount(self.API_URL, adapter)
