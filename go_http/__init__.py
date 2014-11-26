8b720723e7f8caee95908f87d6a0b12ed88b1674
Simon Cross <hodgestar@gmail.com>
2014-11-26 15:17:44 +0200

    Bump version on develop.

--- go_http/__init__.py
+++ go_http/__init__.py
@@ -1,6 +1,6 @@
 """Vumi Go HTTP API client library."""
 
-__version__ = "0.2.1a"
+__version__ = "0.2.3a"
 
 from .send import HttpApiSender, LoggingSender
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="go_http",
-    version="0.2.2a",
+    version="0.2.3a",
     url='http://github.com/praekelt/go-http-api',
     license='BSD',
     description="A client library for Vumi Go's HTTP API",
